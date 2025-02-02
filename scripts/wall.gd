d496a262bffc7dcb1833d117364779af039259f6
David Ward <davidward462@users.noreply.github.com>
2025-02-01 21:32:19 -0800

    change wall speed

--- scripts/wall.gd
+++ scripts/wall.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var speed = 90.0
-var direction = -1
+const DIRECTION = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +10,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# move
-	position.x += speed * delta * direction
+	position.x += speed * delta * DIRECTION
