613896818df50867b7b6a52ddcb69a32a6fcfde6
David Ward <davidward462@users.noreply.github.com>
2025-02-03 15:40:23 -0800

    randomize wall spawn height

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,17 +1,21 @@
 extends Node
 
+var rng = RandomNumberGenerator.new()
 var wall_scene = preload("res://scenes/wall.tscn")
 var wall_spawn_x_pos = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	randomize()
 	
-func create_wall() -> void:
+func create_wall()  -> void:
 	# create upper and lower parts
-	create_block(wall_spawn_x_pos, 550)
-	create_block(wall_spawn_x_pos, 100)
+	var center_height = randf_range(-150, 150)
+	#var center_height = 0
 	
+	create_block(wall_spawn_x_pos, center_height)
+	create_block(wall_spawn_x_pos, center_height+400)
+	 
 func create_block(x_pos, y_pos) -> void:
 	# create instance
 	var instance = wall_scene.instantiate()
@@ -24,5 +28,5 @@ func create_block(x_pos, y_pos) -> void:
 func _process(delta: float) -> void:
 	
 	# for testing only
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_just_pressed("ui_down"):
 		create_wall()
