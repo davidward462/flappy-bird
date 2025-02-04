51ef4891fc226c5e7c959fe2cf2e109ba66f58bd
David Ward <davidward462@users.noreply.github.com>
2025-02-03 17:50:09 -0800

    spawn walls using random offset

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,20 +3,21 @@ extends Node
 var rng = RandomNumberGenerator.new()
 var wall_set_scene = preload("res://scenes/wall_set.tscn")
 var wall_spawn_x_pos = 400
+const wall_spawn_y_offset = 338
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
 	
 func create_wall(x_pos, y_pos)  -> void:
-	#var center_height = randf_range(-200, 200)
-	var center_height = 0
-	# create instance
+	var random_height = randf_range(-100, 100)
+	var center_height = 0 + wall_spawn_y_offset + random_height
+	# create instance 
 	var instance = wall_set_scene.instantiate()
 	add_child(instance)
 	# set position
 	instance.position.x = x_pos
-	instance.position.y = y_pos
+	instance.position.y = y_pos + center_height
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
