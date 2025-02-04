95603e0bac9afb5ea9b0d15b5b8413dc52db3c5f
David Ward <davidward462@users.noreply.github.com>
2025-02-03 17:38:38 -0800

    spawn wall_set instead of wall

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,24 +1,18 @@
 extends Node
 
 var rng = RandomNumberGenerator.new()
-var wall_scene = preload("res://scenes/wall.tscn")
-var wall_spawn_x_pos = 500
+var wall_set_scene = preload("res://scenes/wall_set.tscn")
+var wall_spawn_x_pos = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
 	
-func create_wall()  -> void:
-	# create upper and lower parts
-	var center_height = randf_range(-200, 200)
-	#var center_height = 0
-	
-	create_block(wall_spawn_x_pos, center_height)
-	create_block(wall_spawn_x_pos, center_height+400)
-	 
-func create_block(x_pos, y_pos) -> void:
+func create_wall(x_pos, y_pos)  -> void:
+	#var center_height = randf_range(-200, 200)
+	var center_height = 0
 	# create instance
-	var instance = wall_scene.instantiate()
+	var instance = wall_set_scene.instantiate()
 	add_child(instance)
 	# set position
 	instance.position.x = x_pos
@@ -29,4 +23,4 @@ func _process(delta: float) -> void:
 	
 	# for testing only
 	if Input.is_action_just_pressed("ui_down"):
-		create_wall()
+		create_wall(wall_spawn_x_pos, 0)
