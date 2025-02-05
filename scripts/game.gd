3fee5be6d8512c65b182b3ed52cf6331b8728019
David Ward <davidward462@users.noreply.github.com>
2025-02-04 20:41:45 -0800

    refactor wall creation code

--- scripts/game.gd
+++ scripts/game.gd
@@ -15,17 +15,28 @@ func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
 	randomize()
 	
-func create_wall(x_pos, y_pos)  -> void:
+func calc_y_spawn_pos(y_pos):
 	var random_height = int(randf_range(-100, 100))
 	var center_height = 0 + wall_spawn_y_offset + random_height
 	var y_spawn = y_pos + center_height
+	
+func create_wall(x_pos, y_pos)  -> void:
+	var y_spawn = calc_y_spawn_pos(y_pos)
+	
 	# create instance 
 	var instance = wall_set_scene.instantiate()
+	
 	# set position
 	instance.position.x = x_pos
 	instance.position.y = y_spawn
+	
+	# for debug
 	print("spawned wall at (" + str(x_pos) + ", " + str(y_spawn) + ")")
+	
+	# add child to node tree
 	add_child(instance)
+	
+	# add to group of walls on screen
 	instance.add_to_group("existing_walls")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
