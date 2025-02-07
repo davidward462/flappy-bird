6d1dc49e35e8f892c16ffd99c63882cf81faaa73
David Ward <davidward462@users.noreply.github.com>
2025-02-06 21:05:19 -0800

    add animation for player

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const JUMP_VELOCITY = -600.0
 const GRAVITY = Vector2(0, 2000)
 var alive = true
+@onready var animated_sprite: AnimatedSprite2D = $Sprite2D/AnimatedSprite2D
 
 func _ready() -> void:
 	pass
@@ -12,6 +13,12 @@ func fall() -> void:
 		alive = false
 		velocity.y = 0
 		print("collsion")
+		
+#func _process(delta: float) -> void:
+	#if Input.is_action_pressed("jump"):
+	#	animated_sprite.set_frame(1)
+	#else:
+	#	animated_sprite.set_frame(0)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity
@@ -20,5 +27,6 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and alive:
 		velocity.y = JUMP_VELOCITY
+		
 
 	move_and_slide()
