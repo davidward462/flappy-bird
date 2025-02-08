c3f776ad895a29ac2979985b36db288edeebf9ab
David Ward <davidward462@users.noreply.github.com>
2025-02-08 14:04:53 -0800

    update background sprite

--- scripts/bg_trees.gd
+++ scripts/bg_trees.gd
@@ -1,7 +1,7 @@
 extends ParallaxLayer
 
 var direction = -1
-var speed = 10
+var speed = 7
 
 func _process(delta):
 	motion_offset.x += speed * delta * direction
--- scripts/parallax_background.gd
+++ scripts/parallax_background.gd
@@ -1,7 +1,7 @@
 extends ParallaxBackground
 
 var direction = -1
-var speed = 25
+var speed = 15
 
 func _process(delta):
 	scroll_offset.x += speed * delta * direction
