203d3594b442947bd05ab5d4fa20111c503c6d7a
David Ward <davidward462@users.noreply.github.com>
2025-02-09 17:43:16 -0800

    change to fall sprite on collision

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,14 +13,15 @@ func _ready() -> void:
 func fall() -> void:
 	if alive:
 		caw_sound.play()
+		animated_sprite.set_frame(2)
 		alive = false
 		velocity.y = 0
 		
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("jump"):
-		animated_sprite.set_frame(1)
-	else:
-		animated_sprite.set_frame(0)
+#func _process(delta: float) -> void:
+	#if Input.is_action_pressed("jump"):
+		
+	#else:
+		#
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity
@@ -28,7 +29,11 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and alive:
+		animated_sprite.set_frame(1)
 		velocity.y = JUMP_VELOCITY
 		flap_sound.play()
+	
+	if Input.is_action_just_released("jump") and alive:
+		animated_sprite.set_frame(0)
 		
 	move_and_slide()
